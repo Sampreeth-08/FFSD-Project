1361bf6e064599201e8a0ec2766817b0baa8efce
Sampreeth-08 <sampreeth.jangala1@gmail.com>
2022-03-23 15:17:18 +0530

    login route

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -4,8 +4,6 @@ const router=express.Router();
 const path=require('path')
 
 router.get("/", (req, res, next)=>{
-    console.log(__dirname+"./..views/login.html")
     res.status(200).sendFile(path.join(__dirname, "./../views/login.html"))
-    //res.status(200).sendFile(__dirname+"\\views\\login.html");
 })
 module.exports=router;
